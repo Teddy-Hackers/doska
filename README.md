8d4f01c201371b93cf8b6d9e7e8355d0b9d51bcf
Dmitry Kurtaev <dmitry.kurtaev+github@gmail.com>
2020-07-23 10:35:39 +0300

    show pull requests

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ npm install axios
 then
 
 ```bash
+export PORT=56582
 export GITHUB_CLIENT_ID=client_id
 export GITHUB_CLIENT_SECRET=client_secret
 node server
--- server/index.js
+++ server/index.js
@@ -3,17 +3,82 @@ const url   = require('url');
 const axios = require('axios');
 
 const port = process.env.PORT || 80;
+const org_name = 'Teddy-Hackers'
+
+function github_api_get(url, token, callback) {
+  axios.get(url, {
+    headers: {
+      'Authorization': 'token ' + token,
+      'Accept': 'application/vnd.github.antiope-preview+json'  // for /check-suites
+    }
+  })
+  .then((res) => {
+    callback(res.data);
+  }).catch((err) => {
+    console.error(err);
+  });
+}
 
 function getName(token, callback) {
-  axios.get('https://api.github.com/user', {
+  github_api_get('https://api.github.com/user', token, (data) => {
+    callback(data.login);
+  });
+}
+
+function getStatus(pull, token, callback) {
+  var url = pull.head.repo.url + '/commits/' + pull.head.sha + '/check-suites';
+  github_api_get(url, token, (data) => {
+    callback(data.check_suites[0].conclusion);
+  });
+}
+
+function listPulls(token, repo, user, callback) {
+  axios.get('https://api.github.com/repos/' + user + '/' + repo + '/pulls', {
+    params: {
+      sort: 'created'
+    },
     headers: {
       'Authorization': 'token ' + token
     }
   })
   .then((res) => {
-    callback(res.data.login);
+    var out = [];
+    var pulls = res.data;
+
+    function next(i, callback) {
+      var pull_data = {};
+      pull_data.html_url = pulls[i].html_url;
+      getStatus(pulls[i], token, (data) => {
+        pull_data.status = data;
+        out.push(pull_data);
+
+        if (i < pulls.length - 1) {
+          next(i + 1, callback);
+        } else {
+          callback(out);
+        }
+      });
+    }
+    next(0, callback);
   }).catch((err) => {
     console.error(err);
+  });
+}
+
+function listRepos(token, user_name, callback) {
+  github_api_get('https://api.github.com/user/repos', token, (data) => {
+    data.forEach((repo) => {
+      var name = repo.full_name;
+      github_api_get('https://api.github.com/repos/' + repo.full_name, token, (data) => {
+        if (data.parent && data.parent.owner.login.localeCompare(org_name) == 0) {
+          listPulls(token, repo.name, user_name, (pulls) => {
+            var repos = {};
+            repos[repo.name] = pulls
+            callback(repos);
+          });
+        }
+      });
+    })
   });
 }
 
@@ -36,10 +101,13 @@ http.createServer(function (req, res) {
   .then((oauth_res) => {
     var token_data = url.parse('?' + oauth_res.data, /*parseQueryString*/ true);
     getName(token_data.query.access_token, (name) => {
-      responseData.name = name;
-      console.log(name);
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.end(JSON.stringify(responseData));
+      listRepos(token_data.query.access_token, name, (repos) => {
+        console.log(name);
+        responseData.name = name;
+        responseData.repos = repos;
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.end(JSON.stringify(responseData));
+      });
     });
   }).catch((err) => {
     console.error(err);
